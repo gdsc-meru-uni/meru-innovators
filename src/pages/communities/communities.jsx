faff70cd77cf1b464388d4687f1efc7f0d571707
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-06-17 16:18:07 +0300

    communities page

--- src/pages/communities/communities.jsx
+++ src/pages/communities/communities.jsx
@@ -1,7 +1,28 @@
+import { HiUserGroup } from 'react-icons/hi';
+import CommunityCard from './components/communityCard';
+import Footer from '../../components/footer';
+
 const Communities = () => {
   return (
     <>
-      <h1 className="text-2xl font-bold">Communities</h1>
+      <div className="container px-3">
+        <main className="mb-20">
+          <div className="my-10 flex items-center justify-center gap-8">
+            <HiUserGroup size={70} className="text-green" />
+            <h2 className="text-center text-3xl font-bold text-blue sm:text-5xl">Communitites</h2>
+          </div>
+          <section>
+            <CommunityCard title="Robotics" />
+            <CommunityCard title="Android" />
+            <CommunityCard title="Web Design" />
+            <CommunityCard title="Machine Learning & AI" />
+            <CommunityCard title="Graphic Design" />
+            <CommunityCard title="Cyber Security" />
+            <CommunityCard title="Physics" />
+          </section>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 };
