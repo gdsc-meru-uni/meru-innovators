169f36af18919a4fe14b50acc1d27cc324704f79
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-06-25 18:26:00 +0300

    added other pages

---
+++ src/pages/support/support.jsx
@@ -0,0 +1,11 @@
+const Support = () => {
+  return (
+    <main>
+      <div className="container">
+        <h1 className="text-2xl">Support</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Support;
