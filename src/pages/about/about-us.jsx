cfea436f2c66b76c274c91e2c23861dc0095a955
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-07-01 16:42:40 +0300

    about page

---
+++ src/pages/about/about-us.jsx
@@ -0,0 +1,11 @@
+const AboutUs = () => {
+  return (
+    <main>
+      <div className="container">
+        <h1 className="text-2xl">AboutUs</h1>
+      </div>
+    </main>
+  );
+};
+
+export default AboutUs;
